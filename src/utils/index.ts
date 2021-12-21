48f4e5445fd78c2adfa3dcae648cd291cdbac006
wjh <614618075@qq.com>
2021-12-21 11:26:34 +0800

    压制result[key]的错误

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-export const isFalsy = (value:any) => value === 0 ? false : !value //排除value为0而不传值的情况，value为0也是有效的
+export const isFalsy = (value:unknown) => value === 0 ? false : !value //排除value为0而不传值的情况，value为0也是有效的
 
 // 在一个函数里，改变传入的对象本身的不好的。
 export const cleanObject = (object:object) => {
@@ -23,7 +23,7 @@ export const useMount = (callback:()=> void) => {
     }, [])
 }
 
-export const useDebounce = (value:any, delay?: number) => {
+export const useDebounce = <V>(value:V, delay?: number) => {
     const [debouncedValue, setDebouncedValue] = useState(value)
 
     useEffect(() => {
